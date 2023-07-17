6ba2f1d857a399db497e92c0dbf8c927563ab5af
Christian Dräger <christian.draeger1@gmail.com>
2023-07-17 14:42:29 +0200

    chore: comment

--- proxy/index.ts
+++ proxy/index.ts
@@ -1,2 +1,2 @@
 export * from './first-api/models';
-export * from './second-api/models';
+export * from './second-api/models'; // leads to Module './first-api/models' has already exported a member named 'Error'. Consider explicitly re-exporting to resolve the ambiguity.
